cf15de2c92212d115232924497b4056bc92e5043
Harmit Patel <harmitpatel110@gmail.com>
2026-08-13 23:37:14 -0400

    Agents can make any coding projects.(built own mini cursor).

---
+++ sum.py
@@ -0,0 +1,7 @@
+def add_numbers(a, b):  
+    return a + b  
+  
+if __name__ == '__main__':  
+    a = float(input('Enter first number: '))  
+    b = float(input('Enter second number: '))  
+    print('The sum is:', add_numbers(a, b)) 
