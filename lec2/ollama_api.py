ab3e7605cb881ddc6d8d9854ece9d2ed9b77aee8
Harmit Patel <harmitpatel110@gmail.com>
2026-08-18 22:49:11 -0400

    Model run locally perfectgit add . Docker and FastAPI server uvicorn configured. Fix it on Co-lab now.

--- lec2/ollama_api.py
+++ lec2/ollama_api.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI    #import fastapi
 from ollama import Client      #import ollama
+from fastapi import Body       #import body part from fat api
 
 app=FastAPI()                  #make app of fastapi
 
@@ -14,10 +15,13 @@ client.pull('gemma3:1b')
 @app.post("/chat")             #make route of chat with post call
 #so, if user post anything on /chat route,he/she can chat and need to run ollama on /chat route
 
-def chat():
+
+# pass message parameter as string and write where it comes as an output (here, we have body and write description )
+def chat(message: str = Body(...,description="Chat Message")):
   #now, let's take messages from the user and return whatever response come through model
   response = client.chat(model="gemma3:1b" , messages=[
-    {"role":"user","content":"Hey there!!"}
+    #and remove hardcoded part and make it dynamic as passing messgae to content
+    {"role":"user","content":message}
   ])
 
   return response['message']['content']
